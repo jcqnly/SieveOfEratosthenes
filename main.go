21a44d1158ed130019b3eeea460d4a7bed5ee01a
jackie <jcqnly@gmail.com>
2018-10-28 14:00:14 -0700

    add print lines

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+)
 
 func main() {
 
 	var start uint
 	var end uint
 	flag.UintVar(&start, "start", 0, "usage")
+	flag.UintVar(&end, "end", 10, "usage")
+
+	flag.Parse()
+	//log.Println(start, end)
+	log.Println(start)
+	log.Println(end)
 }
